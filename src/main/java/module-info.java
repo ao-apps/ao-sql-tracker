9e173ddbfd058c3b04c6d8f17bd6a43f1d1a168d
AO Industries, Inc <support@aoindustries.com>
2021-06-29 03:04:03 -0500

    Now supports Java 9+ modules.

---
+++ src/main/java/module-info.java
@@ -0,0 +1,34 @@
+/*
+ * ao-sql-tracker - Tracks JDBC API for unclosed or unfreed objects.
+ * Copyright (C) 2021  AO Industries, Inc.
+ *     support@aoindustries.com
+ *     7262 Bull Pen Cir
+ *     Mobile, AL 36695
+ *
+ * This file is part of ao-sql-tracker.
+ *
+ * ao-sql-tracker is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * ao-sql-tracker is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with ao-sql-tracker.  If not, see <http://www.gnu.org/licenses/>.
+ */
+module com.aoapps.sql.tracker {
+	exports com.aoapps.sql.tracker;
+	provides java.sql.Driver with com.aoapps.sql.tracker.Driver;
+	// Direct
+	requires com.aoapps.collections; // <groupId>com.aoapps</groupId><artifactId>ao-collections</artifactId>
+	requires com.aoapps.collections.transformers; // <groupId>com.aoapps</groupId><artifactId>ao-collections-transformers</artifactId>
+	requires com.aoapps.lang; // <groupId>com.aoapps</groupId><artifactId>ao-lang</artifactId>
+	requires com.aoapps.sql.wrapper; // <groupId>com.aoapps</groupId><artifactId>ao-sql-wrapper</artifactId>
+	// Java SE
+	requires java.logging;
+	requires java.sql;
+}
