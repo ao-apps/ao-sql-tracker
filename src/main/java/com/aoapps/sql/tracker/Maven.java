50f53f9b27c960d9ad90662a8597ecdc18a3b21c
AO Industries, Inc <support@aoindustries.com>
2022-04-16 01:49:27 -0500

    Added blank line before package declarations
    Resolves "EmptyLineSeparator: 'package' should be separated from previous line."

--- src/main/java/com/aoapps/sql/tracker/Maven.java
+++ src/main/java/com/aoapps/sql/tracker/Maven.java
@@ -20,6 +20,7 @@
  * You should have received a copy of the GNU Lesser General Public License
  * along with ao-sql-tracker.  If not, see <https://www.gnu.org/licenses/>.
  */
+
 package com.aoapps.sql.tracker;
 
 import com.aoapps.lang.util.PropertiesUtils;
