57c504a9110a283fdf4be035660551c2f23f34a6
AO Industries, Inc <support@aoindustries.com>
2024-10-12 00:11:28 -0500

    checkstyle-10.18.2 compatibility

--- src/main/java/com/aoapps/sql/tracker/DriverTracker.java
+++ src/main/java/com/aoapps/sql/tracker/DriverTracker.java
@@ -1,6 +1,6 @@
 /*
  * ao-sql-tracker - Tracks JDBC API for unclosed or unfreed objects.
- * Copyright (C) 2020, 2021, 2022  AO Industries, Inc.
+ * Copyright (C) 2020, 2021, 2022, 2024  AO Industries, Inc.
  *     support@aoindustries.com
  *     7262 Bull Pen Cir
  *     Mobile, AL 36695
@@ -48,6 +48,9 @@ public abstract class DriverTracker extends DriverWrapper implements OnCloseHand
 
   private static final Logger logger = Logger.getLogger(DriverTracker.class.getName());
 
+  /**
+   * Creates a new {@link DriverTracker}.
+   */
   protected DriverTracker() {
     // Do nothing
   }
